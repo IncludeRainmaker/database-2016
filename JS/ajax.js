8901d3b2ccc4f3a4b7c98a51e12973e6c0bfa9a2
IncludeRainmaker <a-rademacher@onu.edu>
2016-03-16 20:41:01 -0400

    Updated main ajax function to take a php functions name.

--- JS/ajax.js
+++ JS/ajax.js
@@ -1,4 +1,4 @@
-function theAjax(method, server, username, password)
+function fetch_data_php(method, server, username, password)
 {
 
     return $.ajax({
@@ -8,38 +8,39 @@ function theAjax(method, server, username, password)
         });
 }
 
-function loadDatabasesList() {
-
-  doAjax();
-  return;
-}
-
-function doAjax()
+/**
+ * This function will call the give
+ * @param  {string} php_function [function to run]
+ * @return {[type]}              [description]
+ */
+function call_php(php_function)
 {
-    var server, username, password;
+    var server, username, password, method;
     server = JSON.stringify($('#server').val());
     username = JSON.stringify($('#username').val());
     password = JSON.stringify($('#password').val());
-    ajax = theAjax("getDatabases", server, username, password);
+    method = JSON.stringify()
+    ajax = theAjax(php_function, server, username, password);
     ajax.done(processData);
     ajax.fail(function (){alert("Failure");});
 }
 
+function loadDatabasesList()
+{
+    var php_function = "getDatabases";
+    call_php(php_function);
+    return;
+}
 
 function processData(response_in)
 {
     var response = JSON.parse(response_in);
     var html = "";
-    /*
-    if (response.success)
+
+    $.each (response.data['database_names'], function(key, value)
     {
-        alert("Success in processData");
-    */
-        $.each (response.data['database_names'], function(key, value)
-        {
-            $("#databases")
-                .append($('<option>', {
-                value: value.toString(),
-                text:  value.toString()}));
-        });
+        $("#databases").append($('<option>',{
+            value: value.toString(),
+            text:  value.toString()}));
+    });
 }
